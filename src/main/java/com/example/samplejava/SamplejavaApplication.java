28ef28da5e1232c96156176b8678250fcfc9487c
Yugandhar Sadhu <uggu.sadhu@gmail.com>
2020-04-25 11:12:55 -0400

    first commit

---
+++ src/main/java/com/example/samplejava/SamplejavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplejavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplejava/SamplejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
